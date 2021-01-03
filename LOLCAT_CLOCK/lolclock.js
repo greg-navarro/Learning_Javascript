c678c3a3fa008137512826dd15333918b2e65d7b
genushomollama <gregorylitmannavarro@gmail.com>
2021-01-02 20:30:23 -0800

    completed js functions

--- LOLCAT_CLOCK/lolclock.js
+++ LOLCAT_CLOCK/lolclock.js
@@ -1,31 +1,85 @@
-var chosenNumber = -1;
-var turns = 0;
+// assign variables to components
+// clock display
+var clock = document.getElementById("clockDisplay");
+// alarm
+var alarmButton = document.getElementById("setAlarm");
+var stopAlarmButton = document.getElementById("stopAlarm");
+var alarmHr = document.getElementById("alarmHr");
+var alarmMin = document.getElementById("alarmMin");
+var alarmSec = document.getElementById("alarmSec");
+// image
+var image = document.getElementById("catImage");
 
-function startGame() {
-  let secretNumber = Math.random() % 100;
-  return secretNumber;
+// Build alarm functionality
+// alarm set
+let alarmSound = new Audio("https://freesound.org/data/previews/519/519177_4753243-lq.mp3");
+alarmButton.addEventListener("click", function() {
+  setAlarm();
+});
+stopAlarmButton.addEventListener("click", function() {
+  stopAlarm();
+});
+let checkAlarm = false;
+let hr = alarmHr.innerText;
+let min = alarmMin.innerText;
+let sec = alarmSec.innerText;
 
+function setAlarm() {
+  // Todo validate input
+  checkAlarm = true;
+  hr = alarmHr.value;
+  min = alarmMin.value;
+  sec = alarmSec.value;
+  console.log(hr + min + sec);
 }
 
-function checkMove() {
-  turns = turns + 1;
-  let guess = document.getElementsById('num').value;
-  let message = document.getElementsById('message');
-  if (guess != secretNumber) {
-    message.style.color = "red";
-    if (guess < secretNumber) {
-      message.value = "Wrong Guess, too low, try again";
-    } else {
-      message.value = "Wrong Guess, too high, try again";
+function stopAlarm() {
+  alarmSound.pause();
+  alarmSound.currentTime = 0;
+}
+
+function updateTime() {
+  let dateTimeObj = new Date();
+  let currentHr =  (dateTimeObj.getHours() % 13) + 1;
+  let currentMin = dateTimeObj.getMinutes();
+  let currentSec = dateTimeObj.getSeconds();
+  let newTime = currentHr + ":" + currentMin + ":" + currentSec; 
+  clock.innerText = newTime;
+  console.log("check Alarm is " + checkAlarm);
+  if (checkAlarm) {
+    console.log("The current hour is " + currentHr + " the alarm is " + hr + ":" + min + ":" + sec);
+    // if new time is alarm time then 
+    if (currentHr == hr || currentHr == dateTimeObj.getHours()) {
+      console.log("hr matches");
+      if (currentMin == min) {
+        console.log("min matches");
+        if (currentSec == sec) {
+          console.log("min matches");
+          // sound alarm
+          // let alarmSound = new Audio("https://freesound.org/data/previews/519/519177_4753243-lq.mp3");
+          alarmSound.play();
+          // turn off alarm and reset alarm
+          checkAlarm = false;
+        }
+      }
     }
-    message.value = "Wrong Guess, try again";
-    guess = document.getElementsById('num').value;
+     
   }
-  if (guess == secretNumber) {
-    message.style.color = "green";
-    message.value = "Correct Guess!!!";
-  }
-  message.value = "Correct Guess!!!";
 }
 
-chosenNumber = startGame();
+// Start the clock
+setInterval(function(){ updateTime(); }, 1000);
+
+
+function testAlarm() {
+  let dateTimeObj = new Date();
+  hr = dateTimeObj.getHours();
+  min = dateTimeObj.getMinutes() + 1;
+  sec = dateTimeObj.getSeconds();
+  
+  if (min === 60) {
+    min = 0;
+    hr = (hr + 1) % 24;
+  }
+  checkAlarm = true;
+}
