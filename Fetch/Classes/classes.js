ab9f1a406e5cb435b63430f5f97612a687009369
greg-navarro <gregorylitmannavarro@gmail.com>
2021-10-21 16:41:26 -0700

    Show object inheritance

--- Fetch/Classes/classes.js
+++ Fetch/Classes/classes.js
@@ -2,10 +2,28 @@ class Vacation {
     constructor(dest, length) 
     {
         this.dest = dest;
-        this.lenght = length;
+        this.length = length;
     }
 
     print() {
         console.log(this.dest + " | " + this.length + " days");
     }
 }
+
+const trip = new Vacation("Hell", "forever") // declare with new keyword
+
+// Inheritance is supported
+class Expedition extends Vacation {
+    constructor(dest, length, gear) {
+        super(dest, length);
+        this.gear = gear;
+    }
+
+    print() {
+        super.print();
+        console.log(`Bring your ${this.gear.join(" and ")}`);
+    }
+}
+
+const hike = new Expedition("Everest", 1, ["underwear", "socks"]);
+hike.print();
