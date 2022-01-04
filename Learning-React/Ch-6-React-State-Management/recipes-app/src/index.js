687c0a84882244eafb2a35494260f8d05076cb0f
greg-navarro <gregorylitmannavarro@gmail.com>
2022-01-04 15:30:57 -0800

    more blobs

--- Learning-React/Ch-6-React-State-Management/recipes-app/src/index.js
+++ Learning-React/Ch-6-React-State-Management/recipes-app/src/index.js
@@ -3,4 +3,4 @@ import { render } from "react-dom";
 import Menu from "./components/Menu";
 import data from "./data/recipes.json";
 
-render(<Menu recipes={data} />, document.getElementById("root"))
+render(<Menu recipes={data} />, document.getElementById("root"));
