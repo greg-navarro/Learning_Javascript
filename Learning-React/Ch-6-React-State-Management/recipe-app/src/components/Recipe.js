8b596b01aee57300e3c79a04f83ab7d99534e3bf
greg-navarro <gregorylitmannavarro@gmail.com>
2022-01-06 13:55:02 -0800

    used 'create-react-app' and imported the project files for recipe-app

---
+++ Learning-React/Ch-6-React-State-Management/recipe-app/src/components/Recipe.js
@@ -0,0 +1,17 @@
+import React from "react";
+import IngredientsList from "./IngredientList";
+import Ingredient from "./Ingredient";
+
+export default function Recipe(
+  name = "untitled",
+  ingredients = [],
+  steps = [],
+) {
+  return (
+    <section id={name.toLowerCase().replace(/ /g, "-")}>
+      <h1>{name}</h1>
+      <IngredientsList list={ingredients} />
+      <Ingredient title="Cooking Instructions" steps={steps} />
+    </section>
+  );
+}
